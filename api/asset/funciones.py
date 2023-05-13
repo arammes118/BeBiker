82b98d238347e6fdc80bc0218080bb2567d04666
arammes118 <arammes118@g.educaand.es>
2023-05-14 00:27:37 +0200

    creacion fichero funciones con funcion de respuesta

---
+++ api/asset/funciones.py
@@ -0,0 +1,12 @@
+# IMPORTAMOS LAS CONSTANTES DE ESTADOS DE LA API
+from asset.constantes import *
+
+# FUNCION DE RESPUESTA HTTP
+def respuesta(mensaje):
+    if (mensaje['estado'] == EST_OK): # ESTADO TODO BIEN
+        estado = 200
+    elif (mensaje['estado'] == ERR_NOT_FOUND): # ESTADO DE NO ENCONTRADO
+        estado = 404
+    else: # RESTO DE ERRORES
+        estado = 400
+    return mensaje, estado
