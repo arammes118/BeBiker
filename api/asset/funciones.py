5b78739f8d3aadfa1a09755fac2857a22f1e6100
arammes118 <arammes118@g.educaand.es>
2023-05-18 00:29:29 +0200

    metodo peticion que realiza consultas a la bd(error)

--- api/asset/funciones.py
+++ api/asset/funciones.py
@@ -1,16 +1,34 @@
-# IMPORTAMOS LAS CONSTANTES DE ESTADOS DE LA API
+# Importamos las constantes de los estados de la API
 from asset.constantes import *
+
+#Importamos el fichero JSON para hacer la conexión
+from asset.json import *
+
 from config import DevelopmentConfig
+import requests
 
+# FUNCION DE PETICIONES A LA BD
+def peticion(url, method='GET', json=None):
+    Auth = None if not hasattr(DevelopmentConfig, 'MYSQL_USER') else (DevelopmentConfig.MYSQL_USER, DevelopmentConfig.MYSQL_PASSWORD)
+    url = f"http://{DevelopmentConfig.MYSQL_HOST}:5000{url}"
+    headers =	{
+			"content-type": "application/json",
+			"Accept": "application/json"
+		}
+    payload ={
+		'format':'JSON'
+	}
+    try:
+        if (method=='GET'):
+            return(requests.get(url,
+					headers=headers,
+					params= payload,
+					json=json,
+					auth=Auth,
+					verify=False).json())
+    except:
+        print("ERROR EN LA PETICION")
 
-# CONEXION CON LA BD
-def conex():
-    return conex(
-    user = DevelopmentConfig.MYSQL_USER,
-    password = DevelopmentConfig.MYSQL_PASSWORD,
-    host = DevelopmentConfig.MYSQL_HOST,
-    database = DevelopmentConfig.MYSQL_DB
-    )
 
 # FUNCION DE RESPUESTA HTTP
 def respuesta(mensaje):
@@ -21,24 +39,3 @@ def respuesta(mensaje):
     else: # RESTO DE ERRORES
         estado = 400
     return mensaje, estado
-
-
-# FUNCION DE PETICIONES A LA BD
-def peticion(url,method='GET',json=None):
-	url = f"http://{DevelopmentConfig.MYSQL_HOST}:<puerto>{url}"
-	headers =	{
-			"content-type": "application/json",
-			"Accept": "application/json"
-		}
-	payload ={
-		'format':'JSON'
-	}
-	try:
-		if (method=='GET'):
-			return(requests.get(url,
-					headers=headers,
-					params= payload,
-					json=json,
-					verify=False).json())
-	except:
-		return (None)
---
+++ client/src/context/ConexState.js
@@ -0,0 +1,69 @@
+import React from 'react'
+
+import ConexContext from './ConexContext';
+
+const ConexState = (props) => {
+    const config = require('../config/config.json');
+    /**
+    * lanza una peticion al microservicio
+    * @param {string} url 
+    */
+    const peticion = async (url, {method= 'GET', headers={}, json=null}={}) => {
+        if (json && method==='GET')
+            return ({
+                mensaje: "Imposible mandar JSON con el método GET",
+                estado: 3
+            })
+        let data
+
+        try {
+            let conexBack = config.conexBack
+            let host = conexBack.host
+            let param = {
+                method: method,
+                headers: headers
+            }
+
+            if (json) {
+                param.headers['Content-Type'] = 'application/json'
+                param['body'] = JSON.stringify(json)
+            }
+            if (conexBack?.port??null) //si el puerto es 0 no se usa
+            host+=':'+conexBack.port
+
+            data = await fetch(`${host}/${conexBack.app}/${conexBack.version}${url}`, param)
+
+        } catch {
+            return {
+                mensaje: "El microservicio no responde",
+                estado: 1
+            };
+        }
+
+        if (data.status!==200) {
+            return {
+                mensaje: `Problema de conexión. ${data.status}. ${data.statusText}. ${data.type}`,
+                estado: 2
+            };
+        }
+        data = await data.json()
+        data.peticion = url
+
+        if (config.DebugMode)
+            console.log(data)
+
+        return (data)
+    }
+
+    return (
+        <ConexContext.Provider
+            value={{
+                peticion
+            }}
+        >
+            {props.children}
+        </ConexContext.Provider>
+    );
+};
+
+export default ConexState
