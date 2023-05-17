8f3677fe1cf5d0cbe4896ce9be108acac564544c
arammes118 <arammes118@g.educaand.es>
2023-05-17 23:15:56 +0200

    modioficamos las rutas

--- client/src/App.js
+++ client/src/App.js
@@ -9,12 +9,16 @@ import {
 
 //componentes
 import Login from "./components/Login";
+import Home from './components/Home';
+import Registro from './components/Registro';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path='registro' element={<Registro />} />
       </Routes>
     </BrowserRouter>
   );
