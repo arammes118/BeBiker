c1d5b187f3e9273fd485f10ce1153b95ee660279
arammes118 <arammes118@g.educaand.es>
2023-05-17 14:36:22 +0200

    mods

--- api/config.py
+++ api/config.py
@@ -2,6 +2,7 @@ class DevelopmentConfig():
     DEBUG = True
     MYSQL_HOST = 'localhost'
     MYSQL_USER = 'root'
+    MYSQL_PORT = 3306
     MYSQL_PASSWORD = 'stallman'
     MYSQL_DB = 'BeBiker'
 
