864be512c926a08574ccb00e6bedfed68b26a0ae
arammes118 <arammes118@g.educaand.es>
2023-05-21 00:18:29 +0200

    Constantes Token

--- api/asset/constantes.py
+++ api/asset/constantes.py
@@ -2,5 +2,9 @@
 EST_OK = 0
 ERR_NOT_FOUND = 1
 ERR_PARAM_NEC = 2
-ERR_NO_CONNECT_BD = 3
+ERR_NO_CONNECT_BD = 3 # Problema en la connexion a la BD
+ERR_TOKEN_REQ = 4 # token requerido
+ERR_TOKEN_INV = 5 # token inválido
+ERR_TOKEN_EXP = 6 # token expirado
+ERR_PARAM_ERR = 7 # parametros incorrectos
 ERR_OTHER = 999 #CUALQUIER OTRO ERROR
