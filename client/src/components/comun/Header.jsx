cc10598dd6cfea0c4bd01578d4132e3cba0170ff
arammes118 <arammes118@g.educaand.es>
2023-05-24 14:32:44 +0200

    Header OK

---
+++ client/src/components/comun/Header.jsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import logo from '../../assets/img/BeBiker.png'
+import '../../assets/css/header.css'
+
+export const Header = () => {
+
+    return (
+        <div className='header'>
+            <div className="logo">
+                <img src={logo} alt="BeBiker" />
+            </div>
+            <div className="search-bar">
+                <input type="text" placeholder="Busca un usuario" />
+            </div>
+        </div>
+    )
+
+}
