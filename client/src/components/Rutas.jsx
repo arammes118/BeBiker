5cb96a8402631aa9170db5ad466d29b0d54ad737
arammes118 <arammes118@g.educaand.es>
2023-05-25 16:54:12 +0200

    creacion rutas

--- client/src/components/Rutas.jsx
+++ client/src/components/Rutas.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
+import chema from '../assets/img/chema.jpg'
+
+import '../assets/css/rutas.css'
 
 export default function Rutas() {
   return (
-    <div>Rutas</div>
+    <div class="tarjeta">
+      <div class="contenido">
+        <div class="ladoIzq">
+          <h2 class="titulo">Ruta por el tranco</h2>
+          <div class="cuerpo">
+            <p>Ruta de curvas perfecta</p>
+            <a class="btn" href="#">Más info</a>
+            <a class="btn" href="#">Ver perfil</a>
+          </div>
+        </div>
+        <div class="ladoDer">
+          <img src={chema} /></div>
+      </div>
+    </div>
   )
 }
