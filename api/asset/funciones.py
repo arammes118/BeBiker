8f0262350e7217716c195aaa9ce054a9c34dd3b2
arammes118 <arammes118@g.educaand.es>
2023-06-08 12:53:00 +0200

    mods funciones autentificacion

--- api/asset/funciones.py
+++ api/asset/funciones.py
@@ -17,47 +17,47 @@ from config import DevelopmentConfig
 config = cargaJSON('../config.json')
 
 def conex():
-	return conexDB( 
-		host = config['db']['host'], 
-		user = config['db']['user'],
-		port = config['db']['port'],
-		psw = config['db']['psw'], 
-		db = config['db']['db'] 
+    return conexDB( 
+        host = config['db']['host'], 
+        user = config['db']['user'],
+        port = config['db']['port'],
+        psw = config['db']['psw'], 
+        db = config['db']['db'] 
     )
 
 # FUNCION DE PETICIONES A LA BD
 def peticion(url, method='GET', json=None):
     Auth = None if 'user' not in config['db'] else (config['db']['user'], config['db']['psw'])
     url = f"http://{config['db']['host']}:{config['db']['port']}{url}"
-    headers =	{
-			"content-type": "application/json",
-			"Accept": "application/json"
-		}
+    headers =    {
+            "content-type": "application/json",
+            "Accept": "application/json"
+        }
     payload ={
-		'format':'JSON'
-	}
+        'format':'JSON'
+    }
     try:
         if (method=='GET'):
             return(requests.get(url,
-					headers=headers,
-					params= payload,
-					json=json,
-					auth=Auth,
-					verify=False).json())
+                    headers=headers,
+                    params= payload,
+                    json=json,
+                    auth=Auth,
+                    verify=False).json())
         elif (method=='POST'):
             return(requests.post(url,
-					headers=headers,
-					params= payload,
-					json=json,
-					auth=Auth,
-					verify=False).json())
+                    headers=headers,
+                    params= payload,
+                    json=json,
+                    auth=Auth,
+                    verify=False).json())
         elif (method=='DELETE'):
             return(requests.delete(url,
-					headers=headers,
-					params= payload,
-					json=json,
-					auth=Auth,
-					verify=False).json())
+                    headers=headers,
+                    params= payload,
+                    json=json,
+                    auth=Auth,
+                    verify=False).json())
     except:
         return (None)
 
@@ -71,46 +71,41 @@ def respuesta(mensaje):
         estado = 400
     return mensaje, estado
 
-# FUNCION QUE DEVUELVE UN JWT codificado
+# devuelve un jwt codificado localmente y con exp y iat diferentes
 def getJWT(pay):
-    pay['iat'] = round(time.time())
-    pay['exp'] = pay['iat'] + config['api']['expToken'] # expiracion del token
-    return (jwt.encode(pay, config['api']['secretJWT'], algorithm='HS256'))
+    pay['iat']=round(time.time())
+    pay['exp']=pay['iat']+config['api']['expToken']#expiración en segundos
+    return (jwt.encode(pay, config['api']['secretJWT'], algorithm="HS256"))
 
-# FUNCIÓN AUTENTIFICA EL JWT
+#comprueba que se está autentificando con los JWT correctos
 def autentificacion():
-    token = request.headers.get('authorization')
-    # comprobamos que se le pase el token por el header
-    if (token == None):
+    token=request.headers.get('authorization')
+    if (token==None):# se necesita este argumento 
         return ({
-            'estado': ERR_TOKEN_REQ,
-            'mensaje': (f'Token requerido'),
-            'token': ''
+            'estado':ERR_TOKEN_REQ, 
+            'mensaje':(f"Token requerido"),
+            'token':''
         })
-    
     try:
-        # Decodificamos el token pasado con la claveJWT que la encripta para validar que es correcta
-        payload = jwt.decode(token, config['api']['secretJWT'], algorithm='HS256') 
+        payload=jwt.decode(token, config['api']['secretJWT'], algorithms="HS256")
+    except jwt.ExpiredSignatureError:#el jwt ha expirado
+        return ({
+            'estado':ERR_TOKEN_EXP, 
+            'mensaje':(f"Token expirado"),
+            'token':''
+        })
 
-    except jwt.ExpiredSignatureError: # Nuestro token ha expirado
-        return ({
-            'estado': ERR_TOKEN_EXP,
-            'mensaje': (f'Token expirado'),
-            'token': ''
+    except jwt.InvalidTokenError:
+        return ({ 
+            'estado':ERR_TOKEN_INV, 
+            'mensaje':(f"Token inválido"),
+            'token':''
         })
-    
-    except jwt.InvalidTokenError: # El token no es correcto
-        return ({
-            'estado': ERR_TOKEN_INV,
-            'mensaje': (f'Token inválido'),
-            'token': ''
-        })
-    
-    res = {
-        'estado': EST_OK,
-        'mensaje': 'OK'
+    res={
+        'estado':EST_OK, 
+        'mensaje':"OK",
+        'perfil_id': payload.get('id', False), # Obtenemos el valor de admin desde el payload del token
     }
-
     if (round(time.time()) - payload['iat'] > config['api']['renToken']): # Hay que renovar el token
         res['token'] = getJWT(payload)
     return (res)
