dea82fe0ad435bbd6753ef2690e9dcc106842e86
arammes118 <arammes118@g.educaand.es>
2023-06-08 12:51:33 +0200

    listado de rutas

--- client/src/components/Rutas.jsx
+++ client/src/components/Rutas.jsx
@@ -1,23 +1,78 @@
-import React from 'react'
-import chema from '../assets/img/chema.jpg'
+// Importaciones REACT
+import React, { useContext, useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
 
-import '../assets/css/rutas.css'
+// Importación Contexto
+import ConexContext from '../context/ConexContext'
+
+// CSS
+import '../assets/css/publicaciones.css'
+import '../assets/css/post.css'
+
+// Logo
+import ruta from '../assets/img/ruta.jpg'
+import fer from '../assets/img/fer.jpg'
+
+// CARD POST MATERIAL UI
+import Card from '@mui/material/Card'
+import CardHeader from '@mui/material/CardHeader'
+import CardContent from '@mui/material/CardContent'
+import CardMedia from '@mui/material/CardMedia'
+import Avatar from '@mui/material/Avatar'
+import { Box } from '@mui/material'
+
 
 export default function Rutas() {
+  const { peticion, perfil_id } = useContext(ConexContext) // Contexto
+
+  // Estados
+  const [List, setList] = useState([]) //listado de publicaciones
+
+  // UseEffects
+  useEffect(() => {
+    async function listar() {
+      const pet = await peticion("/rutas") // Peticion de publicaciones
+      setList(pet)
+      console.log(pet)
+    }
+
+    listar()
+  }, [perfil_id, peticion])
+
+
   return (
-    <div className="tarjeta">
-      <div className="contenido">
-        <div className="ladoIzq">
-          <h2 className="titulo">Ruta por el tranco</h2>
-          <div className="cuerpo">
-            <p>Ruta de curvas perfecta</p>
-            <a className="btn" href="#">Más info</a>
-            <a className="btn" href="#">Ver perfil</a>
-          </div>
-        </div>
-        <div className="ladoDer">
-          <img src={chema} /></div>
-      </div>
+    <div className='contentRuta'>
+      {List.map((elem) => (
+        <Card key={elem.idRuta} style={{ width: '100%', marginBottom: '20px', background: '#292929' }}>
+          <CardHeader
+            avatar={
+              <Avatar src={fer} />
+            }
+            title={
+              <span style={{ fontWeight: 'bold', color: 'white' }}>{elem.usuario}</span>
+            } />
+          <CardMedia
+            component="img"
+            width="100%"
+            height="100px"
+            image={ruta}
+          />
+          <h2 className='titulo'>{elem.titulo}</h2>
+          <p className='descripcion'>{elem.descripcion}</p>
+          <CardContent>
+            <Box display="flex" alignItems="center">
+              <div className='actionBtn'>
+              <Link to={`/ruta/${elem.titulo}/${elem.usuario}`}>
+                  <button className='btnRutas'>Más informacion</button>
+                </Link>
+                <Link to={`/perfil/${elem.usuario}`}>
+                  <button className='btnRutas'>Ver perfil</button>
+                </Link>
+              </div>
+            </Box>
+          </CardContent>
+        </Card>
+      ))}
     </div>
   )
 }
